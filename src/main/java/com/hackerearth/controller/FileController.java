dedb21f8f44c5cde0a61a2f61b2baa70134abfaa
sharath <48129125+sharathshiva@users.noreply.github.com>
2022-11-06 13:29:24 +0530

    Net Cracker Updated Code

--- src/main/java/com/hackerearth/controller/FileController.java
+++ src/main/java/com/hackerearth/controller/FileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://net-cracker-hackerearth.herokuapp.com/")
 @RestController
 @RequestMapping("/api/v1/files")
 public class FileController {
